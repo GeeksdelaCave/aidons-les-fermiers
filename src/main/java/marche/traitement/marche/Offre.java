0da3c157cd3a65703fedc0069e9de48429285698
Frolz83 <frolz83@gmail.com>
2016-11-28 17:16:25 +0100

    Ajout de la classe offre et descriptif des méthodes à implémenter

---
+++ src/main/java/marche/traitement/marche/Offre.java
@@ -0,0 +1,45 @@
+package marche.traitement.marche;
+
+/**
+ * Classe Offre
+ *
+ */
+public class Offre {
+
+    /**
+     * Nom du fermier à qui appartient l'offre
+     */
+    //TODO ajouter donnée membre fermier
+
+    /**
+     * Nom de l'acheteur
+     */
+    //TODO ajouter donnée membre acheteur
+
+    /**
+     *  Le prix de l'offre
+     */
+    private float prix;
+
+    /**
+     * Nom du produit
+     */
+    //TODO ajouter donnée membre produits
+
+    /**
+     * Constructeur de la classe
+     */
+    //TODO constructeur
+
+    /**
+     * Méthode valider
+     */
+    //TODO ajouter la méthode valider
+
+    /**
+     * Méthode acheter
+     */
+    //TODO ajouter la méthode acheter
+
+
+}
