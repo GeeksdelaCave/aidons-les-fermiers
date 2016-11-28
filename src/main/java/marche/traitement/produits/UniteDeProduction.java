d504d588da085daa1dba74289b22940f2193fccd
Romain <romain.colonna.distria@etu.univ-amu.fr>
2016-11-28 17:55:22 +0100

    Suppression fichier inutile Ajout class UniteDeProduction

---
+++ src/main/java/marche/traitement/produits/UniteDeProduction.java
@@ -0,0 +1,13 @@
+package marche.traitement.produits;
+
+
+public class UniteDeProduction {
+
+    /**
+     * capacité de production (en nombre de lots vendable, ex : 1 lait = 3l de lait)
+     */
+    private int capaciteProduction;
+
+
+
+}
