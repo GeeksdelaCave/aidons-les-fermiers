6ab9bf920cb2d7b2411c5e6a67e0b3d162ce7fee
Frolz83 <frolz83@gmail.com>
2016-11-29 19:43:22 +0100

    Ajout des TODO dans la classe MarcheBasique

--- src/main/java/marche/traitement/marche/MarcheBasique.java
+++ src/main/java/marche/traitement/marche/MarcheBasique.java
@@ -4,4 +4,8 @@ package marche.traitement.marche;
  * Classe MarcheBasique
  */
 public class MarcheBasique implements IMarche {
+
+    //TODO Ajouter les attributs
+    //TODO Ajouter les méthodes
+
 }
