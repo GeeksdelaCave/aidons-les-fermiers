54d07b1b464a0ae973181b003de910cadc602042
Frolz83 <frolz83@gmail.com>
2016-11-30 09:13:12 +0100

    Ajout des méthodes choisirAcheteur(), transfertBiens() et crediterSomme() ainsi que leur documentation

--- src/main/java/marche/traitement/marche/Controleur.java
+++ src/main/java/marche/traitement/marche/Controleur.java
@@ -4,12 +4,43 @@ package marche.traitement.marche;
  * Singleton Controleur représentant le système
  */
 public class Controleur {
+    /**
+     * Instance du Singleton
+     */
     private static Controleur ourInstance = new Controleur();
 
+    /**
+     * Permet de récuperer l'instance unique car notre classe est un Singleton
+     * @return l'instance unique
+     */
     public static Controleur getInstance() {
         return ourInstance;
     }
 
+    /**
+     * Constructeur privé
+     */
     private Controleur() {
+    }
+
+    /**
+     * Méthode permettant de selectionner les acheteurs
+     */
+    public void choisirAcheteur(){ //prend en paramètre : acheteurs Collection<IAcheteur>
+        //TODO implémenter la méthode
+    }
+
+    /**
+     * Méthode permettant le transfert de biens
+     */
+    public void transfererBiens(){ // prend en paramètre : offre Offre
+        //TODO  implémenter la méthode
+    }
+
+    /**
+     * Méthode permettant de créditer une somme par rapport à une offre
+     */
+    public void crediterSomme(){ // prend en paramètre offre Offre
+        //TODO implémenter la méthode
     }
 }
