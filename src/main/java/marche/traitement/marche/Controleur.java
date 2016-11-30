0d24044d73b65e03b83596bca3834c714343a673
Frolz83 <frolz83@gmail.com>
2016-11-30 09:22:06 +0100

    Ajout des méthodes regulerPrix() et ajouterOffre() ainsi que leur documentation

--- src/main/java/marche/traitement/marche/Controleur.java
+++ src/main/java/marche/traitement/marche/Controleur.java
@@ -40,7 +40,21 @@ public class Controleur {
     /**
      * Méthode permettant de créditer une somme par rapport à une offre
      */
-    public void crediterSomme(){ // prend en paramètre offre Offre
+    public void crediterSomme(){ // prend en paramètre : offre Offre
+        //TODO implémenter la méthode
+    }
+
+    /**
+     * Méthode permettant de réguler les prix des produits en fonction du seuil définit
+     */
+    public void regulerPrix(){ // prend en paramètre : produit ProduitFermier
+        //TODO implémenter la méthode
+    }
+
+    /**
+     * Méthode permettant d'ajouter une offre à un marché
+     */
+    public void ajouterOffre(){ //prend en paramètre : offre Offre, marche Marche
         //TODO implémenter la méthode
     }
 }
