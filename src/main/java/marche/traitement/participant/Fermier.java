413205b8cf14420fe21e2f9110f5cc92ce00569b
Romain <romain.colonna.distria@etu.univ-amu.fr>
2016-12-05 09:07:41 +0100

    Ajout des TODO

--- src/main/java/marche/traitement/participant/Fermier.java
+++ src/main/java/marche/traitement/participant/Fermier.java
@@ -1,34 +1,48 @@
 package marche.traitement.participant;
 
 
+import marche.traitement.produits.ProduitFermier;
+import marche.traitement.produits.UniteDeProduction;
+import java.util.Collection;
+
 public class Fermier extends Acteur{
 
     /**
      * Liste des produits fermiers possédé par le fermier
      */
-    //TODO ajouter donnée membre inventaire
+    private Collection<ProduitFermier> inventaire;
 
     /**
      * Liste des unités de productions possédé par le fermier
      */
-    //TODO ajouter donnée membre unitesDeProduction
+    private Collection<UniteDeProduction> uniteDeProductions;
 
     /**
      * Constructeur par défault de la classe
      */
     public Fermier () {
-        //TODO compléter constructeur par défault
+        inventaire = null;
+        uniteDeProductions = null;
         solde = 0;
     }
 
     /**
      * Constructeur du fermier avec caractéristiques
+     * @param inventaire Liste de produis fermier à attribuer au fermier lors de sa création
+     * @param uniteDeProductions Liste des unités de production attribuées au fermier lors de sa création
      * @param solde Solde à attribuer au fermier lors de sa création
      */
-    public Fermier (float solde) {
-        //TODO compléter constructeur
+    public Fermier (Collection<ProduitFermier> inventaire, Collection<UniteDeProduction> uniteDeProductions, float solde) {
+        this.inventaire = inventaire;
+        this.uniteDeProductions = uniteDeProductions;
         this.solde = solde;
     }
 
-    //TODO Ajouter méthode d'ajout de produit dans l'inventaire
+    /**
+     * Méthode d'ajout de produit fermier dans l'inventaire
+     * @param produit Produit à ajouter a l'inventaire du fermier
+     */
+    public void ajoutProduitFermier(ProduitFermier produit) {
+        inventaire.add(produit);
+    }
 }
