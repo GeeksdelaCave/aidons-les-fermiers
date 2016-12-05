793eb0eb8fc815edda324f972333cc57f82f20bb
ThibaudCenentG2A2 <peter2070@hotmail.fr>
2016-12-05 15:55:40 +0100

    Prise en compte modifications tests unitaires du à suppression données membres dans classes Lait,Pomme,Vache

--- src/test/java/marche/traitement/produits/VacheTest.java
+++ src/test/java/marche/traitement/produits/VacheTest.java
@@ -9,7 +9,7 @@ import java.time.Month;
 
 /**
  * VacheTest désigne le test unitaire effectué sur la classe Vache sur tous ces getter() crées
- * @version 1.2
+ * @version 1.3
  */
 public class VacheTest {
 
@@ -51,10 +51,10 @@ public class VacheTest {
     public void testGetIsCommercialise()
     {
         Vache vache = new Vache(150.0f,LocalDate.of(2016, Month.NOVEMBER,28), (short) 55.0, "Landes", 1200,"vachetest",50.0f);
-        assertTrue(vache.isCommercialise() == true);
+        assertTrue(vache.isCommercialisable() == true);
 
         Vache vache1 = new Vache(150.0f,LocalDate.of(2016, Month.NOVEMBER,28), (short) 20.0, "Landes", 1200,"vachetest",50.0f);
-        assertTrue(vache1.isCommercialise() == false);
+        assertTrue(vache1.isCommercialisable() == false);
     }
 
     /**
