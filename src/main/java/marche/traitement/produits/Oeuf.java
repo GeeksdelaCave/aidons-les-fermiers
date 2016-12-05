ff9ddb0e24ac0f60b9ec01d9f6b704a34f8d4da7
Romain <romain.colonna.distria@etu.univ-amu.fr>
2016-12-05 10:15:52 +0100

    Création de la classe Oeuf

--- src/main/java/marche/traitement/produits/Oeuf.java
+++ src/main/java/marche/traitement/produits/Oeuf.java
@@ -1,7 +1,28 @@
 package marche.traitement.produits;
 
+import java.time.LocalDate;
+
 /**
- * Created by romain on 05/12/2016.
+ * Classe Fromage représentant un fromage
+ * @version 1.1
  */
-public class Oeuf {
+public class Oeuf extends ProduitFermier {
+    /**
+     * L'identifiant d'un pack d'oeufs Il n'est pas modifiable
+     */
+    private int idPackOeuf;
+
+    /**
+     * Constructeur d'un Oeuf
+     *
+     * @param prix           correspond au pris du pack d'oeufs
+     * @param datePeremption désigne la date de péremption des oeufs contenu dans le pack
+     * @param qualite        désigne la qualité du produit sur une échelle de 1 à 100
+     * @param lieuCreation   correspond au lieu de ponte des oeufs
+     * @param idPackOeuf     correspond a l'identifiant du pack d'oeuf
+     */
+    public Oeuf(float prix, LocalDate datePeremption, short qualite, String lieuCreation, int idPackOeuf) {
+        super(prix, datePeremption, qualite, lieuCreation);
+        this.idPackOeuf = idPackOeuf;
+    }
 }
