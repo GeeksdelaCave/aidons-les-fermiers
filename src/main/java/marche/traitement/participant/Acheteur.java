a88d1d6b4332746d8932dbb2d0c520135932efef
Frolz83 <frolz83@gmail.com>
2016-12-06 16:16:42 +0100

    Ajout du corps de la classe acteur ainsi que sa méthode proposerOffreAchat()

---
+++ src/main/java/marche/traitement/participant/Acheteur.java
@@ -0,0 +1,10 @@
+package marche.traitement.participant;
+
+/**
+ * Classe Acheteur
+ */
+public class Acheteur extends Decorateur {
+    public void proposerOffreAchat(/* int quantite, produitFermier produit*/){
+        //TODO implémenter la méthode
+    }
+}
