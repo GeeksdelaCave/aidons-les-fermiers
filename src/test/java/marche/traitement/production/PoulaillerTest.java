1c14c0a78891c459926538676abc6d74df8ef1d2
GeeksdelaCave <geeksdelacave@laposte.net>
2016-12-08 17:53:12 +0100

    Test des œufs et màj de l'unité de production. Intégration de l'exception InventairePleinException

--- src/test/java/marche/traitement/production/PoulaillerTest.java
+++ src/test/java/marche/traitement/production/PoulaillerTest.java
@@ -1,10 +1,12 @@
 package marche.traitement.production;
 
+import marche.traitement.exceptions.InventairePleinException;
 import marche.traitement.produits.Oeuf;
-import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 import java.time.LocalDate;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Classe PoulaillerTest représentant les tests unitaires effectués sur la classe
@@ -15,7 +17,7 @@ public class PoulaillerTest {
     /**
      * Teste de créer deux packs d'oeufs et vérifie leur identification à la création
      */
-    /*@Test
+    @Test
     public void testCreerOeuf() {
         Poulailler poulailler = new Poulailler(10,"Limoges");
         Oeuf packoeuf = poulailler.creerPackOeuf(3.0f, LocalDate.of(2016,10,28),(short)50.0);
@@ -23,13 +25,13 @@ public class PoulaillerTest {
 
         Oeuf packoeuf2 = poulailler.creerPackOeuf(4.0f,LocalDate.of(2016,10,28),(short) 75.0);
         assertTrue(packoeuf2.getIdPackOeuf() == 200);
-    }//*/
+    }
 
     /**
      *  Teste si l'ajout à l'inventaire est possible
      */
-    @Test
-    public void testAjouterInventaire() {
+    @Test (expected = InventairePleinException.class)
+    public void testAjouterInventaire() throws InventairePleinException {
         Poulailler poulailler = new Poulailler(1,"Limoges");
         Oeuf packoeuf = poulailler.creerPackOeuf(3.0f, LocalDate.of(2016,10,28),(short)50.0);
 
