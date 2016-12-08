14994d2bfd2a2f00ce8d5c2963e8d1861fbf7d74
Frolz83 <frolz83@gmail.com>
2016-12-08 17:36:51 +0100

    Ajout des paramètre de la méthode proposerOffreAchat() de la classe Acheteur

--- src/main/java/marche/traitement/participant/Acheteur.java
+++ src/main/java/marche/traitement/participant/Acheteur.java
@@ -1,10 +1,12 @@
 package marche.traitement.participant;
 
+import marche.traitement.produits.ProduitFermier;
+
 /**
  * Classe Acheteur
  */
 public class Acheteur extends Decorateur {
-    public void proposerOffreAchat(/* int quantite, produitFermier produit*/){
+    public void proposerOffreAchat( int quantite, ProduitFermier produit){
         //TODO implémenter la méthode
     }
 }
