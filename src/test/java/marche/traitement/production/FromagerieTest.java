fff5ec52426d89d80d943e74c3567c7ac6900733
Romain <romain.colonna.distria@etu.univ-amu.fr>
2016-12-12 13:15:04 +0100

    Ajout de tests pour la classe Fromagerie

--- src/test/java/marche/traitement/production/FromagerieTest.java
+++ src/test/java/marche/traitement/production/FromagerieTest.java
@@ -1,7 +1,45 @@
 package marche.traitement.production;
 
-/**
- * Created by Thibaud on 06/12/2016.
- */
+import marche.traitement.produits.Fromage;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+
 public class FromagerieTest {
+
+    @Test
+    public void testCreerfromage () {
+        Fromagerie fromagerie = new Fromagerie(100, "PACA");
+        Fromage fromage = fromagerie.creerFromage(110, LocalDate.of(2100, Month.SEPTEMBER, 12), (short)96, "Camember", 245);
+
+        assert (fromagerie != null);
+    }
+
+    @Test
+    public void testCreerfromage_verifIdFromage () {
+        Fromagerie fromagerie = new Fromagerie(100, "PACA");
+        Fromage fromage1 = fromagerie.creerFromage(110, LocalDate.of(2100, Month.SEPTEMBER, 12), (short)96, "Roquefort", 245);
+        Fromage fromage2 = fromagerie.creerFromage(110, LocalDate.of(2100, Month.SEPTEMBER, 12), (short)96, "Emmentale", 300);
+
+        assert (fromage1.getIdFromage() != fromage2.getIdFromage());
+    }
+
+    @Test
+    public void testCreerfromage_inventaireIsNotNull () {
+        Fromagerie fromagerie = new Fromagerie(100, "PACA");
+        Fromage fromage1 = fromagerie.creerFromage(110, LocalDate.of(2100, Month.SEPTEMBER, 12), (short)96, "Jean-Luc", 245);
+        Fromage fromage2 = fromagerie.creerFromage(110, LocalDate.of(2100, Month.SEPTEMBER, 12), (short)96, "Robert", 300);
+
+        assert (fromagerie.getInventaireUniteDeProduction() != null);
+    }
+
+    @Test
+    public void testCreerfromage_tailleInventaireEgal2 () {
+        Fromagerie fromagerie = new Fromagerie(100, "PACA");
+        Fromage fromage1 = fromagerie.creerFromage(110, LocalDate.of(2100, Month.SEPTEMBER, 12), (short)96, "Jean-Luc", 245);
+        Fromage fromage2 = fromagerie.creerFromage(110, LocalDate.of(2100, Month.SEPTEMBER, 12), (short)96, "Robert", 300);
+
+        assert (fromagerie.getInventaireUniteDeProduction().size() == 2);
+    }
 }
