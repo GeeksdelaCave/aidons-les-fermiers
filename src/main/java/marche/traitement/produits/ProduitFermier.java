15b8671ac0eed3364ba1cafbca4535db5fdc2348
ThibaudCenentG2A2 <peter2070@hotmail.fr>
2016-12-16 14:55:51 +0100

    Fin implémentation de la méthode validerLbael() Ajout de la donnée membre propriétaire correspondant à un Fermier dans Produit Fermier ainsi que getter et setter() correspodants

--- src/main/java/marche/traitement/produits/ProduitFermier.java
+++ src/main/java/marche/traitement/produits/ProduitFermier.java
@@ -3,7 +3,9 @@ package marche.traitement.produits;
 import marche.traitement.exceptions.ProduitPerimeException;
 import marche.traitement.label.Label;
 import marche.traitement.label.LabelAOC;
+import marche.traitement.label.LabelAOP;
 import marche.traitement.label.LabelRouge;
+import marche.traitement.participant.Fermier;
 import marche.traitement.production.UniteDeProduction;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -13,10 +15,14 @@ import java.util.HashMap;
  *
  *Classe abstraite ProduitFermier représentant une idée d'un produit fermier
  *@author Thibaud CENENT
- * @version 2.0
+ * @version 2.1
  */
 public abstract class ProduitFermier {
 
+    /**
+     * Map qui associe le type d'un produit à une région particulière pour l'attribution des labels. Elle n'est pas modifiable
+     * @see ProduitFermier#validerLabel()
+     */
     private static final HashMap<String, String> associationProduitRegion = new HashMap<String, String>() {{
        put("Pomme", "Bretagne");
        put("Fromage", "Normandie");
@@ -26,6 +32,12 @@ public abstract class ProduitFermier {
        put("Vache", "PACA");
        put("Cochon", "Corse");
     }};
+
+    /**
+     * Fermier propriétaire d'un produit fermier. Il n'est pas modifiable.
+     */
+    private Fermier proprietaire;
+
     /**
      * le prix du produit.Il n'est pas modifiable
      *
@@ -48,7 +60,12 @@ public abstract class ProduitFermier {
      */
     protected short qualite;
 
-    
+    /**
+     * Identifiant d'un label qui s'inceémentera au fur et à mesure de l'attribution d'un label. Il n'est pas modifiable
+     * @see ProduitFermier#validerLabel()
+     */
+    private static int idLabel = 1;
+
     /**
      * Unité de production qui est propre à un produit fermier. Elle n'est pas modifiable
      * @see ProduitFermier#getUniteDeProduction()
@@ -115,12 +132,36 @@ public abstract class ProduitFermier {
      * Valider le choix d'un label en fonction des caractéristiques du produit fermier
      */
     public void validerLabel() {
-        if(associationProduitRegion.get(this.getClass().getCanonicalName()) == this.getUniteDeProduction().getRegionCreationProduit()) {
-            if(this.isCommercialisable())
-                this.ajouterLabel(new LabelAOC(12,this.getUniteDeProduction().getRegionCreationProduit(),this.isCommercialisable()));
+        if(this.getDatePeremption() != null)
+        {
+            if(associationProduitRegion.get(this.getClass().getCanonicalName()) == this.getUniteDeProduction().getRegionCreationProduit()) {
+                if (this.isCommercialisable())
+                    this.ajouterLabel(new LabelAOC(idLabel, this.getUniteDeProduction().getRegionCreationProduit(), this.isCommercialisable()));
+                ++idLabel;
+            }
+            if(this.getClass().getCanonicalName() == this.getUniteDeProduction().getRegionCreationProduit())
+                this.ajouterLabel(new LabelAOP(idLabel, this.getUniteDeProduction().getRegionCreationProduit()));
+            ++idLabel;
             if(this.getQualite() > 70)
-                this.ajouterLabel(new LabelRouge(15,true));
+                this.ajouterLabel(new LabelRouge(idLabel,true));
+            ++idLabel;
         }
+    }
+
+    /** Retourne le propriétaire actuel du produit fermier
+     *
+     * @return le propriétaite actuel du produit fermier
+     */
+    public Fermier getProprietaire() {
+        return proprietaire;
+    }
+
+    /** Met à jour le propriétaire courant à chaque vente d'un produit fermier
+     *
+     * @param proprietaire désignant le propriétaire courant d'un produit fermier
+     */
+    public void setProprietaire(Fermier proprietaire) {
+        this.proprietaire = proprietaire;
     }
 
     /** Ajoute un label à un produit fermier
