1ebb10a85d362ffc739a4e885dc51b02244c4c5c
GeeksdelaCave <geeksdelacave@laposte.net>
2016-12-30 17:09:56 +0100

    Ajout de documentation de l'acheteur

--- src/main/java/marche/traitement/participant/Acheteur.java
+++ src/main/java/marche/traitement/participant/Acheteur.java
@@ -1,27 +1,38 @@
 package marche.traitement.participant;
 
 import marche.traitement.marche.Offre;
-import marche.traitement.produits.ProduitFermier;
 
 /**
  * Classe Acheteur
+ *
+ * Un acheteur est un décorateur d'Acteur, qui permet d'implémenter la méthode proposerOffreAchat(...).
+ *
  * @author Nicolas Guigou
+ *
+ * @version 1.1
+ *
+ * @see Acteur
+ * @see Acheteur#proposerOffreAchat(Offre)
  */
 public class Acheteur extends Decorateur {
 
     /**
-     * Constructeur
-     * @param acteur
+     * Constructeur d'un acheteur.
+     * @param acteur L'acteur qui va pouvoir proposer une offre d'achat.
+     *
+     * @see Acteur
      */
     public Acheteur(Acteur acteur){
         super(acteur);
     }
 
     /**
-     * Permet de proposer une offre d'achat
+     * Permet de proposer une offre d'achat.
      * @param offre
+     *
+     * @see Offre
      */
-    public void proposerOffreAchat( Offre offre){
+    public void proposerOffreAchat(Offre offre){
         offre.ajouterAcheteur(this);
     }
 }
