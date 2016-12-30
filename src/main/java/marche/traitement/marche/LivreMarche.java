1632f3f18c0873facf74d145f260962b9bf1e31e
Frolz83 <frolz83@gmail.com>
2016-12-30 17:28:26 +0100

    Ajout de l'attribut historique de la classe LivreMarche

--- src/main/java/marche/traitement/marche/LivreMarche.java
+++ src/main/java/marche/traitement/marche/LivreMarche.java
@@ -1,8 +1,15 @@
 package marche.traitement.marche;
 
+import java.util.ArrayList;
+
 /**
  * Classe LivreMarche
  * @author Nicolas Guigou
  */
 public class LivreMarche {
+
+    /**
+     * Historique de toute les transactions
+     */
+    private static ArrayList<String> historique;
 }
