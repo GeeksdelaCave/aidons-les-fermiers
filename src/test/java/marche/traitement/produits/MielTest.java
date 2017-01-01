57c3dcf6fe840f750857322ee538e588ac3b508e
Romain <romain.colonna.distria@etu.univ-amu.fr>
2017-01-01 21:49:03 +0100

    Ajout de tests dans la classe MielTest

--- src/test/java/marche/traitement/produits/MielTest.java
+++ src/test/java/marche/traitement/produits/MielTest.java
@@ -1,46 +1,103 @@
 package marche.traitement.produits;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
+import marche.traitement.exceptions.ProduitPerimeException;
 import marche.traitement.production.Ruche;
 import org.junit.Test;
 
 import java.time.LocalDate;
 import java.time.Month;
 
+import static org.junit.Assert.*;
+
 /**
  * MielTest désigne les tests unitaires effectués sur la classe Miel
  * @version 1.4
  */
 public class MielTest {
+
     /**
-     * Teste l'identifiant d'un seau de miel
+     * Test de l'identifiant d'un pot de miel
      */
     @Test
-    public void testGetIdMiel()
-    {
-        Miel miel = new Miel(15.0f,LocalDate.of(2016, Month.NOVEMBER,30),(short) 55.0,  1000, "Mieldechataignier",new Ruche(20,"Loire"));
-        assertTrue(miel.getIdSeauMiel() == 1000);
+    public void testGetIdPotMiel() {
+        Miel potMiel = new Miel(10.0f, LocalDate.of(2016, Month.NOVEMBER, 28), (short) 55, 0.2f, "Abeille", new Ruche(30, "PACA"));
+        assertTrue(potMiel.getIdSeauMiel() == 1500);
     }
 
     /**
-     * Teste le poids d'un seau de miel
+     * Test du poids d'un pot de miel
      */
     @Test
-    public void testGetPoidsSeauMiel()
-    {
-        Miel miel = new Miel(15.0f,LocalDate.of(2016, Month.NOVEMBER,30), (short) 55.0,  1000, "Mieldechataignier", new Ruche(20,"Loire"));
-        assertTrue(miel.getPoidsSeauMiel() == 5.0f);
+    public void testGetPoidsPotMiel() {
+        Miel potMiel = new Miel(10.0f, LocalDate.of(2016, Month.NOVEMBER, 28), (short) 55, 0.2f, "Abeille", new Ruche(30, "PACA"));
+        assertTrue(potMiel.getPoidsSeauMiel() == 0.2f);
     }
 
     /**
-     * Teste le type d'un seau de miel
+     * Test du type d'un pot de miel
      */
     @Test
-    public void testGetTypeMiel()
-    {
-        Miel miel = new Miel(15.0f,LocalDate.of(2016, Month.NOVEMBER,30), (short) 55.0,  1000, "Mieldechataignier", new Ruche(20,"Loire"));
-        assertEquals(miel.getTypeDeMiel(), "Mieldechataignier");
+    public void testGetTypeDeMiel() {
+        Miel potMiel = new Miel(10.0f, LocalDate.of(2016, Month.NOVEMBER, 28), (short) 55, 0.2f, "Abeille", new Ruche(30, "PACA"));
+        assertEquals(potMiel.getTypeDeMiel(), "Abeille");
+    }
+
+    /**
+     * Test du prix d'un pot de miel
+     */
+    @Test
+    public void testGetPrix() {
+        Miel potMiel = new Miel(10.0f, LocalDate.of(2016, Month.NOVEMBER, 28), (short) 55, 0.2f, "Abeille", new Ruche(30, "PACA"));
+        assertTrue(potMiel.getPrix() == 10.0f);
+    }
+
+    /**
+     * Test de la date de péremption du pot de miel
+     */
+    @Test
+    public void testGetDatePeremption() {
+        Miel potMiel = new Miel(10.0f, LocalDate.of(2100, Month.NOVEMBER, 28), (short) 55, 0.2f, "Abeille", new Ruche(30, "PACA"));
+        assertEquals(LocalDate.of(2100, Month.NOVEMBER, 28), potMiel.getDatePeremption());
+    }
+
+    /**
+     * Teste de l'exception ProduitPerimeException lors de l'accés à la date de péremption
+     *
+     * @see ProduitPerimeException
+     */
+    /*
+    @Test(expected = ProduitPerimeException.class)
+    public void testGetDatePeremption_ProduitPerimeException() throws ProduitPerimeException {
+        Miel potMiel = new Miel(10.0f, LocalDate.of(2016, Month.NOVEMBER, 18), (short) 55, 0.2f, "Abeille", new Ruche(30, "PACA"));
+        assertEquals(LocalDate.of(2016, Month.NOVEMBER, 30), potMiel.getDatePeremption());
+    }
+    */
+
+    /**
+     * Test de la qualité du pot de miel
+     */
+    @Test
+    public void testGetQualite() {
+        Miel potMiel = new Miel(10.0f, LocalDate.of(2016, Month.NOVEMBER,28), (short) 55, 0.2f, "Abeille", new Ruche(30, "PACA"));
+        assertEquals(55, potMiel.getQualite());
+    }
+
+    /**
+     * Test de l'unité de production du pot de miel
+     */
+    @Test
+    public void testGetUniteDeProduction() {
+        Ruche ruche = new Ruche(150, "Alsace");
+        Miel potMiel = new Miel(10.0f, LocalDate.of(2016, Month.NOVEMBER,28), (short) 55, 0.2f, "Abeille", ruche );
+        assertSame(ruche, potMiel.getUniteDeProduction());
+    }
+
+    /**
+     * Teste si le pot de miel est commercialisable
+     */
+    @Test
+    public void testGetIsCommercialise() throws ProduitPerimeException {
+        Miel potMiel = new Miel(10.0f, LocalDate.of(2100, Month.NOVEMBER,28), (short) 55, 0.2f, "Abeille", new Ruche(30, "PACA"));
+        assertTrue(potMiel.isCommercialisable());
     }
 }
