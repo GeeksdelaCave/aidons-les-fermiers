cd464923ab36d8d3e3c7421b8f0f156fc71a6f68
Romain <romain.colonna.distria@etu.univ-amu.fr>
2017-01-03 00:02:50 +0100

    Correction tests lié à l'identification des vaches

--- src/test/java/marche/traitement/produits/VacheTest.java
+++ src/test/java/marche/traitement/produits/VacheTest.java
@@ -7,47 +7,46 @@ import org.junit.Test;
 import java.time.LocalDate;
 import java.time.Month;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
  * VacheTest désigne le test unitaire effectué sur la classe Vache sur tous ces getter() crées
  *
- * @author Thibaud CENENT
+ *
+ * @author Romain COLONNA D'ISTRIA
  * @author Tristan DIETZ
  *
  * @version 1.7
  */
 public class VacheTest {
 
+    Vache vache1 = new Vache(150.0f,LocalDate.of(2100, Month.NOVEMBER,28), (short) 55.0, "vachetest",50.0f, new Etable(30,"Aquitaine"));
+    Vache vache2 = new Vache(150.0f,LocalDate.of(2000, Month.NOVEMBER,28), (short) 55.0, "vachetest",50.0f, new Etable(30,"Aquitaine"));
+
     /**
      * Teste le prix de la vache
      */
     @Test
-    public void testGetPrix()
-    {
-        Vache vache = new Vache(150.0f,LocalDate.of(2100, Month.NOVEMBER,28), (short) 55.0, "vachetest",50.0f, new Etable(30,"Aquitaine"));
-        assertTrue(vache.getPrix() == 150.0f);
+    public void testGetPrix() {
+        assertTrue(vache1.getPrix() == 150.0f);
     }
 
     /**
      * Teste la date de peremption de la vache
      */
     @Test (expected = ProduitPerimeException.class)
-    public void testGetDatePeremption()
-    {
-        Vache vache = new Vache(150.0f,LocalDate.of(2000, Month.NOVEMBER,28), (short) 55.0, "vachetest",50.0f, new Etable(30,"Aquitaine"));
-        LocalDate ld = LocalDate.of(2016, Month.NOVEMBER,28);
-        assertTrue(vache.getDatePeremption().equals(ld));
+    public void testGetDatePeremption() {
+        LocalDate ld = LocalDate.of(2000, Month.NOVEMBER,28);
+        assertEquals(vache2.getDatePeremption(), ld);
     }
 
     /**
      * Teste la valeur de la qualité de la vache
      */
     @Test
-    public void testGetQualite()
-    {
-        Vache vache = new Vache(150.0f,LocalDate.of(2100, Month.NOVEMBER,28), (short) 55.0, "vachetest",50.0f, new Etable(30,"Aquitaine"));
-        assertTrue(vache.getQualite() == (short) 55.0);
+    public void testGetQualite() {
+        assertTrue(vache1.getQualite() == (short) 55.0);
     }
 
     /**
@@ -55,8 +54,7 @@ public class VacheTest {
      */
     @Test (expected = ProduitPerimeException.class)
     public void testGetIsCommercialise() throws ProduitPerimeException {
-        Vache vache = new Vache(150.0f,LocalDate.of(2000, Month.NOVEMBER,28), (short) 55, "vachetest",50.0f, new Etable(1245, "PACA"));
-        assertTrue(vache.isCommercialisable());
+        assertTrue(vache2.isCommercialisable());
     }
 
 
@@ -64,29 +62,23 @@ public class VacheTest {
      * Teste l'identifiant de la vache
      */
     @Test
-    public void testGetIdVache()
-    {
-        Vache vache = new Vache(150.0f,LocalDate.of(2100, Month.NOVEMBER,28), (short) 55.0, "vachetest",50.0f, new Etable(30,"Aquitaine"));
-        assertTrue(vache.getIdVache() == 3000 || vache.getIdVache() == 3005); //car idVacheGeneral = 3000;
+    public void testGetIdVache() {
+        assertTrue(vache1.getIdVache() == 3000 || vache1.getIdVache() == 3800);
     }
 
     /**
      * Teste le nom de la vache
      */
     @Test
-    public void testGetNomVache()
-    {
-        Vache vache = new Vache(150.0f,LocalDate.of(2100, Month.NOVEMBER,28), (short) 55.0, "vachetest",50.0f, new Etable(30,"Aquitaine"));
-        assertTrue(vache.getNomVache() == "vachetest");
+    public void testGetNomVache() {
+        assertTrue(vache1.getNomVache() == "vachetest");
     }
 
     /**
      * Teste le poids de la vache
      */
     @Test
-    public void testGetPoidsVache()
-    {
-        Vache vache = new Vache(150.0f,LocalDate.of(2100, Month.NOVEMBER,28), (short) 55.0, "vachetest",50.0f, new Etable(30,"Aquitaine"));
-        assertTrue(vache.getPoidsVache() == 50.0f);
+    public void testGetPoidsVache() {
+        assertTrue(vache1.getPoidsVache() == 50.0f);
     }
 }
