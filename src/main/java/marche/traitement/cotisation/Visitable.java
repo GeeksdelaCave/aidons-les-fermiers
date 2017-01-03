3e5589331cd0ab07757ebacf716a2bb7de9d4031
ThibaudCenentG2A2 <peter2070@hotmail.fr>
2017-01-03 22:39:24 +0100

    Ajout corps des interfaces Cotisation et Visitable, de la classe CotisationGenerale

--- src/main/java/marche/traitement/cotisation/Visitable.java
+++ src/main/java/marche/traitement/cotisation/Visitable.java
@@ -1,7 +1,16 @@
 package marche.traitement.cotisation;
 
 /**
- * Created by Thibaud on 03/01/2017.
+ * Interface permettant de gérer le payement de la cotisation d'un fermier.
+ * @author Thibaud CENENT
+ * @version 1.1
  */
 public interface Visitable {
+
+    /**
+     *
+     * @param cotisation désigne la cotisation que va devoir payer un fermier en fonction de ses caractéristiques.
+     * @return le solde du fermier qui a été crédité de la cotisation.
+     */
+    public double payerCotisation(Cotisation cotisation);
 }
