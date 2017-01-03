21d44092a8947c3667a68bd18b60aabdc38173d0
GeeksdelaCave <geeksdelacave@laposte.net>
2017-01-03 21:04:30 +0100

    Correction d'un test : erreur de code obsolète.

--- src/test/java/marche/traitement/participant/FermierTest.java
+++ src/test/java/marche/traitement/participant/FermierTest.java
@@ -20,7 +20,7 @@ public class FermierTest {
      * Test du solde du trader avec le bon type
      */
     public void testDuSoldeDuFermier() {
-        Acteur fermier = new ProducteurLaitier();
+        Acteur fermier = new ProducteurLaitier(null, null, 0);
         fermier.ajouterSolde(50.0);
 
         assert fermier.getSolde() == 50.0;
@@ -31,7 +31,7 @@ public class FermierTest {
      * Test du solde du trader avec un int (test du cast implicite)
      */
     public void testDuSoldeDuFermierAvecInt() {
-        Acteur fermier = new Horticulteur();
+        Acteur fermier = new Horticulteur(null, null, 0);
         fermier.ajouterSolde(50);
 
         assert fermier.getSolde() == 50.0;
@@ -42,7 +42,7 @@ public class FermierTest {
      * Test d'enlever du solde supérieur à ce qui est possible : soulève une exception SoldeNonDisponibleException
      */
     public void testEnleverSolde() throws SoldeNonDisponibleException {
-        Acteur fermier = new ProducteurDeViande();
+        Acteur fermier = new ProducteurDeViande(null, null, 0);
         fermier.ajouterSolde(30.0);
         fermier.enleverSolde(50.0);
     }
@@ -52,7 +52,7 @@ public class FermierTest {
      * Test d'ajout du produit fermier dans l'inventaire d'un acteur
      */
     public void testAjouterProduit() {
-        Acteur fermier = new Arboriculteur();
+        Acteur fermier = new Arboriculteur(new ArrayList<ProduitFermier>(), null, 0);
         ProduitFermier pomme = new Pomme(10, LocalDate.of(1996, Month.SEPTEMBER, 13), (short)90, 1, "Reinette", 15, new Verger(50,"Normandie"));
 
 
