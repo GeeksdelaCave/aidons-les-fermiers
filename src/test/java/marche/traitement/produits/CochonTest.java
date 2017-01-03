4430993111dc6aacff140f1874871e1e7a3afb61
Romain <romain.colonna.distria@etu.univ-amu.fr>
2017-01-03 18:04:45 +0100

    Correction et ajout de tests

--- src/test/java/marche/traitement/produits/CochonTest.java
+++ src/test/java/marche/traitement/produits/CochonTest.java
@@ -15,19 +15,24 @@ import static org.junit.Assert.*;
  * @author Thibaud CENENT
  * @author Tristan DIETZ
  *
- * @version 1.5
+ * @version 1.6
  * @see Cochon
  * @see EnclosCochon
  */
 public class CochonTest {
 
+    EnclosCochon encloCochon = new EnclosCochon(150,"Alsace");
+    Cochon cochonNonPerime = new Cochon(150.0f, LocalDate.of(2100, Month.NOVEMBER, 28), (short) 55, 5.0f, "Cochondelait", encloCochon);
+    Cochon cochonPerime    = new Cochon(150.0f, LocalDate.of(2000, Month.NOVEMBER, 28), (short) 55, 5.0f, "Cochondelait", encloCochon);
+
     /**
-     * Teste l'identifiant d'un cochon
+     * Teste l'identifiant d'un cochon.
+     * La première condition sert pour quand le test est lancé seul, la seconde
+     * pour quand tout les tests sont lancés ensembles.
      */
     @Test
     public void testGetIdCochon() {
-        Cochon cochon = new Cochon(150.0f, LocalDate.of(2016, Month.NOVEMBER, 30), (short) 55, 5.0f, "Cochondelait", new EnclosCochon(150, "Alsace"));
-        assertTrue(cochon.getIdCochon() == 2000 || cochon.getIdCochon() == 2900); //car idCochonGeneral = 2000 initialement
+        assertTrue(cochonNonPerime.getIdCochon() == 2000 || cochonNonPerime.getIdCochon() == 2900); //car idCochonGeneral = 2000 initialement
     }
 
     /**
@@ -35,8 +40,7 @@ public class CochonTest {
      */
     @Test
     public void testGetPoidsCochon() {
-        Cochon cochon = new Cochon(150.0f, LocalDate.of(2016, Month.NOVEMBER, 30), (short) 55, 5.0f,"Cochondelait", new EnclosCochon(150,"Alsace"));
-        assertTrue(cochon.getPoidsCochon() == 5.0f);
+        assertTrue(cochonNonPerime.getPoidsCochon() == 5.0f);
     }
 
     /**
@@ -44,8 +48,7 @@ public class CochonTest {
      */
     @Test
     public void testGetTypeCochon() {
-        Cochon cochon = new Cochon(150.0f, LocalDate.of(2016, Month.NOVEMBER, 30), (short) 55, 5.0f, "Cochondelait", new EnclosCochon(150,"Alsace"));
-        assertEquals("Cochondelait", cochon.getTypeDeCochon());
+        assertEquals("Cochondelait", cochonNonPerime.getTypeDeCochon());
     }
 
     /**
@@ -53,8 +56,7 @@ public class CochonTest {
      */
     @Test
     public void testGetPrix() {
-        Cochon cochon = new Cochon(150.0f, LocalDate.of(2016, Month.NOVEMBER, 30), (short) 55, 5.0f, "Cochondelait", new EnclosCochon(150,"Alsace"));
-        assertTrue(cochon.getPrix() == 150.0f);
+        assertTrue(cochonNonPerime.getPrix() == 150.0f);
     }
 
     /**
@@ -62,8 +64,7 @@ public class CochonTest {
      */
     @Test
     public void testGetDatePeremption() {
-        Cochon cochon = new Cochon(150.0f, LocalDate.of(2100, Month.NOVEMBER, 30), (short) 55, 5.0f,"Cochondelait", new EnclosCochon(150,"Alsace"));
-        assertEquals(LocalDate.of(2100, Month.NOVEMBER, 30), cochon.getDatePeremption());
+        assertEquals(LocalDate.of(2100, Month.NOVEMBER, 28), cochonNonPerime.getDatePeremption());
     }
 
     /**
@@ -73,8 +74,7 @@ public class CochonTest {
      */
     @Test (expected = ProduitPerimeException.class)
     public void testGetDatePeremption_ProduitPerimeException() throws ProduitPerimeException {
-        Cochon cochon = new Cochon(150.0f, LocalDate.of(2000, Month.NOVEMBER, 30), (short) 55, 5.0f,"Cochondelait", new EnclosCochon(150,"Alsace"));
-        assertEquals(LocalDate.of(2100, Month.NOVEMBER, 30), cochon.getDatePeremption());
+        assertEquals(LocalDate.of(2100, Month.NOVEMBER, 30), cochonPerime.getDatePeremption());
     }
 
     /**
@@ -82,8 +82,7 @@ public class CochonTest {
      */
     @Test
     public void testGetQualite() {
-        Cochon cochon = new Cochon(150.0f, LocalDate.of(2016, Month.NOVEMBER, 30), (short) 55, 5.0f,"Cochondelait", new EnclosCochon(150,"Alsace"));
-        assertEquals(55, cochon.getQualite());
+        assertEquals(55, cochonNonPerime.getQualite());
     }
 
     /**
@@ -91,8 +90,22 @@ public class CochonTest {
      */
     @Test
     public void testGetUniteDeProduction() {
-        EnclosCochon encloCochon = new EnclosCochon(150,"Alsace");
-        Cochon cochon = new Cochon(150.0f, LocalDate.of(2016, Month.NOVEMBER, 30), (short) 55, 5.0f, "Cochondelait", encloCochon);
-        assertSame(encloCochon, cochon.getUniteDeProduction());
+        assertSame(encloCochon, cochonNonPerime.getUniteDeProduction());
+    }
+
+    /**
+     * Teste si le cochon est commercialisable
+     */
+    @Test
+    public void testGetIsCommercialise() {
+        assertTrue(cochonNonPerime.isCommercialisable());
+    }
+
+    /**
+     * Teste de l'exception ProduitPerimeException lors de la vérification pour la commercialisation
+     */
+    @Test (expected = ProduitPerimeException.class)
+    public void testGetIsCommercialise_ProduitPerimeException() throws ProduitPerimeException {
+        assertTrue(cochonPerime.isCommercialisable());
     }
 }
