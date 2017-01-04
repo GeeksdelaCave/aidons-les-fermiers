e2cf4aafbd501115bd38b2e444e13eb8fe92001d
ThibaudCenentG2A2 <peter2070@hotmail.fr>
2017-01-04 20:35:51 +0100

    Création du constructeur de FenetreCreationOffreVente avec la mise en place des différents composants nécessaires pour créer une offre de vente

--- src/main/java/marche/affichage/FenetreCreationOffreVente.java
+++ src/main/java/marche/affichage/FenetreCreationOffreVente.java
@@ -2,6 +2,7 @@ package marche.affichage;
 
 import marche.traitement.cotisation.Visitable;
 
+import javax.swing.*;
 import java.awt.*;
 
 /**
@@ -10,7 +11,43 @@ import java.awt.*;
 public class FenetreCreationOffreVente extends FenetreGenerale implements Observable {
 
     public FenetreCreationOffreVente(String title) throws HeadlessException {
+
         super(title);
+
+        JPanel panneauCreationOffreVente = new JPanel();
+        panneauCreationOffreVente.setLayout(new BorderLayout());
+
+        JLabel produitFermier = new JLabel("Produit Fermier");
+        String produitsfermiers[] = {"Cochon","Fromage","Lait","Miel","Oeuf","Pomme","Vache"};
+        JComboBox comboBoxInventaireVendeur = new JComboBox(produitsfermiers);
+
+        JLabel quantite = new JLabel("Quantité : ");
+        JTextField zoneTexteQuantite = new JTextField();
+
+        JLabel prix = new JLabel("Prix : ");
+        JTextField zoneTextePrix = new JTextField();
+
+        JButton validation = new JButton("Valider");
+
+        JPanel panneauCorps = new JPanel();
+        panneauCorps.setLayout(new GridLayout(7,0,10,7));
+        panneauCorps.setBorder(BorderFactory.createEmptyBorder(8,8,8,8));
+        panneauCorps.add(produitFermier);
+        panneauCorps.add(comboBoxInventaireVendeur);
+        panneauCorps.add(quantite);
+        panneauCorps.add(zoneTexteQuantite);
+        panneauCorps.add(prix);
+        panneauCorps.add(zoneTextePrix);
+        panneauCorps.add(validation);
+        panneauCreationOffreVente.add(panneauCorps);
+
+
+        this.setSize(300,300);
+        this.setContentPane(panneauCreationOffreVente);
+        this.setVisible(true);
+
+
+
     }
 
     public void ajouterObservateur(Observateur observateur) {
@@ -23,5 +60,9 @@ public class FenetreCreationOffreVente extends FenetreGenerale implements Observ
 
     public void notifierObservateur() {
 
+    }
+
+    public static void main(String[] args){
+        new FenetreCreationOffreVente("Creer Offre de Vente");
     }
 }
