33871a4a39924468f588e3a845e64a0168b62112
GeeksdelaCave <geeksdelacave@laposte.net>
2017-01-04 21:34:52 +0100

    Création de la classe des popups :D

---
+++ src/main/java/marche/affichage/ExceptionsPopups.java
@@ -0,0 +1,14 @@
+package marche.affichage;
+
+/**
+ * La liste des popups d'erreur affichées en cas d'exception.
+ *
+ * @author Tristan DIETZ
+ *
+ * @version 1.0
+ * @see marche.traitement.exceptions.InventairePleinException
+ * @see marche.traitement.exceptions.ProduitPerimeException
+ * @see marche.traitement.exceptions.SoldeNonDisponibleException
+ */
+public class ExceptionsPopups {
+}
