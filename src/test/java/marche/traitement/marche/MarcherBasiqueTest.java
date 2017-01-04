b370bda5d0f6a9468c0cdc3eafd83244dcc37e35
Romain <romain.colonna.distria@etu.univ-amu.fr>
2017-01-04 21:04:01 +0100

    Ajout classe test MarcherBasiqueTest. Ajout de test dans la classe DecorateurTest.

---
+++ src/test/java/marche/traitement/marche/MarcherBasiqueTest.java
@@ -0,0 +1,79 @@
+package marche.traitement.marche;
+
+import marche.traitement.participant.Acheteur;
+import marche.traitement.participant.Horticulteur;
+import marche.traitement.participant.ProducteurDeViande;
+import marche.traitement.participant.Vendeur;
+import marche.traitement.production.Ruche;
+import marche.traitement.production.UniteDeProduction;
+import marche.traitement.produits.Miel;
+import marche.traitement.produits.ProduitFermier;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Tests unitaire de la classe MarcheBasique.
+ *
+ * @author Romain COLONNA D'ISTRIA
+ *
+ * @see MarcheBasique
+ *
+ * @version 1.0
+ */
+public class MarcherBasiqueTest {
+
+    @Test
+    public void testAjouterOffre() {
+        ArrayList<ProduitFermier> pf = new ArrayList<ProduitFermier>();
+        ArrayList<ProduitFermier> pf2 = new ArrayList<ProduitFermier>();
+
+        ArrayList<UniteDeProduction> udp = new ArrayList<UniteDeProduction>();
+
+        Ruche ruche = new Ruche(100, "PACA");
+        Miel miel = new Miel(40, LocalDate.of(2100, Month.NOVEMBER, 28), (short) 89, 1, "abeilles", ruche);
+
+        udp.add(ruche);
+        pf.add(miel);
+
+        Acheteur acheteur = new Acheteur(new Horticulteur(pf, udp, 1000));
+        Vendeur vendeur = new Vendeur(new ProducteurDeViande(pf2, null, 1000));
+
+        Offre offre = new Offre(40, pf, vendeur);
+
+        MarcheBasique marche = new MarcheBasique("Bazard");
+        marche.ajouterOffre(offre);
+
+        assertTrue(marche.getCatalogue().contains(offre));
+    }
+
+    @Test
+    public void testEnleverOffre() {
+        ArrayList<ProduitFermier> pf = new ArrayList<ProduitFermier>();
+        ArrayList<ProduitFermier> pf2 = new ArrayList<ProduitFermier>();
+
+        ArrayList<UniteDeProduction> udp = new ArrayList<UniteDeProduction>();
+
+        Ruche ruche = new Ruche(100, "PACA");
+        Miel miel = new Miel(40, LocalDate.of(2100, Month.NOVEMBER, 28), (short) 89, 1, "abeilles", ruche);
+
+        udp.add(ruche);
+        pf.add(miel);
+
+        Acheteur acheteur = new Acheteur(new Horticulteur(pf, udp, 1000));
+        Vendeur vendeur = new Vendeur(new ProducteurDeViande(pf2, null, 1000));
+
+        Offre offre = new Offre(40, pf, vendeur);
+
+        MarcheBasique marche = new MarcheBasique("Bazard");
+        marche.ajouterOffre(offre);
+        marche.enleverOffre(offre);
+
+        assertTrue(marche.getCatalogue().isEmpty());
+    }
+
+}
