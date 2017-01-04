09d20f17ebb3dc4bde02802fc4b8b8272ae8d610
Frolz83 <nicolas.guiogu83@gmail.com>
2017-01-04 16:02:11 +0100

    Implémentation de la classe FenetreCreationParticipant

--- src/main/java/marche/affichage/FenetreCreationParticipant.java
+++ src/main/java/marche/affichage/FenetreCreationParticipant.java
@@ -1,9 +1,40 @@
 package marche.affichage;
 
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import java.awt.*;
+
+
 
 /**
  * @author Nicolas Guigou
  */
 public class FenetreCreationParticipant extends JFrame {
+
+    private String acteurs[] = {"Fermier","Traider","Grossiste","Centrale d'achat"};
+    private JPanel panneauCreationParticipant = new JPanel();
+    private JComboBox comboBox = new JComboBox(acteurs);
+
+    private JLabel label1 = new JLabel("Choisir votre type d'acteur");
+
+    public FenetreCreationParticipant(String titre)
+    {
+        super(titre);
+        panneauCreationParticipant.setLayout(new BorderLayout());
+        JPanel panneau = new JPanel();
+        panneau.setLayout(new GridLayout(4,1));
+        panneauCreationParticipant.add(panneau);
+
+        panneau.add(label1);
+        panneau.add(comboBox);
+
+        this.setSize(300,300);
+        this.setContentPane(panneauCreationParticipant);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new FenetreCreationParticipant("Fenêtre de création de participant");
+    }
+
 }
