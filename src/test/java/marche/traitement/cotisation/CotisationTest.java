cf041b19e9e05191149e0f9c091b21f914314c41
ThibaudCenentG2A2 <peter2070@hotmail.fr>
2017-01-04 10:59:36 +0100

    Modification du chiffre multiplicateur de la méthode calculerCotisation() pour Arboriculteur Modification tests unitaires sur Cotisation et nouveau solde fermier après cotisation enlevée

--- src/test/java/marche/traitement/cotisation/CotisationTest.java
+++ src/test/java/marche/traitement/cotisation/CotisationTest.java
@@ -1,12 +1,31 @@
 package marche.traitement.cotisation;
 
+import marche.traitement.participant.Arboriculteur;
+import marche.traitement.participant.ProducteurDeViande;
 import org.junit.Test;
 
 public class CotisationTest {
 
     @Test
+    /**
+     * Test du calcul du montant de la cotisation pour un producteur de viande
+     */
     public void testCalculMontantCotisationProducteurDeViande()
     {
+        ProducteurDeViande producteurDeViande = new ProducteurDeViande(null,null,150.0);
+        double cotisationAPayer = producteurDeViande.payerCotisation(new CotisationGenerale());
+        assert cotisationAPayer == 3.0;
+    }
 
+
+    @Test
+    /**
+     *  Test du calcul du montant de la cotisation pour un arboculteur
+     */
+    public void testCalculMontantCotisationArboriculteur()
+    {
+        Arboriculteur arboriculteur = new Arboriculteur(null,null,200.0);
+        double cotisationAPayer = arboriculteur.payerCotisation(new CotisationGenerale());
+        assert cotisationAPayer == 10.0;
     }
 }
