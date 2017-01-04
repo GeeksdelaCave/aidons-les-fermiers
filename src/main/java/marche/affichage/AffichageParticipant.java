0a35b5234eeca9113557c828713a2df4ed089ff7
ThibaudCenentG2A2 <peter2070@hotmail.fr>
2017-01-04 13:29:23 +0100

    Création fenetre principale avec affichage des participants

--- src/main/java/marche/affichage/AffichageParticipant.java
+++ src/main/java/marche/affichage/AffichageParticipant.java
@@ -1,9 +1,37 @@
 package marche.affichage;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 /**
  * Created by Thibaud on 04/01/2017.
  */
-public class AffichageParticipant {
+public class AffichageParticipant extends JFrame {
+    private JPanel panneauAffichageParticipant = new JPanel();
+    private JMenuBar barreMenu = new JMenuBar();
+    private JMenu menu = new JMenu("Menu");
+    private ArrayList<JMenuItem>  menuItems = new ArrayList<JMenuItem>();
+
+    public AffichageParticipant(String titre)
+    {
+        super(titre);
+        panneauAffichageParticipant.setLayout(new BorderLayout());
+        menuItems.add(new JMenuItem("Creer Participant"));
+        menuItems.add(new JMenuItem("Paramètres"));
+        menuItems.add(new JMenuItem("Documentation"));
+        menuItems.add(new JMenuItem("A propos de "));
+        menuItems.add(new JMenuItem("Quitter l'application"));
+        for(JMenuItem jMenuItem : menuItems)
+            menu.add(jMenuItem);
+        barreMenu.add(menu);
+        this.setJMenuBar(barreMenu);
+        this.setContentPane(panneauAffichageParticipant);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new AffichageParticipant("Fenêtre principal");
+    }
 }
